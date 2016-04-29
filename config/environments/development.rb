a1170c5a59858d15840ec7d50502f1aa8528dafb
Collin <cjames59@gmail.com>
2016-04-29 05:54:07 -0500

    added host url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #  host: 'localhost', port: 3000
+  config.action_mailer.default_url_options = { https://github.com/cjames59/Blog }
 end
